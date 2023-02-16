0bf15a28f5e07d7bc60a3c280c5774941712d935
Lee-Snyder2022 <l_snyder2@massbay.edu>
2023-02-16 12:48:55 -0500

    Coding Day 2

--- src/Clue.java
+++ src/Clue.java
@@ -1,13 +1,21 @@
 public class Clue {
-    private int[][] elements;
+    private int[] elements;
+    private int compare;
     private String story;
     private int type;
+    private int size;
 
-    public Clue(int t){
+    public Clue(int t, int s){
         type = t;
+        size = s;
     }
 
-    public void setElements(int n){
+    public void setElements(int[] e){
+        elements = e;
+    }
+
+    public void setCompare(int c){
+        compare = c;
     }
 
     public int getType(){
--- src/LogicGame.java
+++ src/LogicGame.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class LogicGame {
     private int gameTime;
@@ -13,11 +14,28 @@ public class LogicGame {
     }
     public void startGame(int people, int values){
         int gridCount = (values*(values+1))/2;
-        ArrayList<Integer[][]> grids = new ArrayList<>();
+        ArrayList<Cell[][]> grids = new ArrayList<>();
         for(int i = 0; i <gridCount; i++){
-            grids.add(new Integer[people][people]);
+            grids.add(new Cell[people][people]);
         }
-
+        ArrayList<Clue> clues = new ArrayList<>();
+        Random rand = new Random();
+        do{
+            int type = rand.nextInt(4);
+            clues.add(new Clue(type, people));
+            switch (type){
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    System.out.println("error");
+            }
+        }while (false);
     }
     public boolean checkSolution(){
         return true;
